c9fbe3f0661cd9cf153bdfdb3e23ab738bfbfffb
AYLCE2OK <ahayoleonce62@gmail.com>
2024-10-13 09:04:56 -0700

    Update play.php

--- play.php
+++ play.php
@@ -1,14 +1,1 @@
-<?php
-$servername = "localhost";
-$username = "root";  // Default MySQL username
-$password = "";  // Default MySQL password, change if you have set a different password
-$dbname = "play";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
